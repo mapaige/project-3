8fbe731baa46477cb1b437b74413144e88ff3c9b
Marcia Paige <mapaige7@gmail.com>
2020-10-26 21:38:40 -0400

    Added files and font family

---
+++ project3/src/App.js
@@ -0,0 +1,16 @@
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+
+
+  function App() {
+    return (
+   <div className="App">
+       <h1> Testing</h1>
+      </div>
+    );
+  }
+
+  export default App;
+
+
+
